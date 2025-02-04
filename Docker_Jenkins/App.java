6fb431c99f160d860693433d04486407918e1a61
sai <sai@gmail.com>
2025-02-04 14:26:38 +0530

    updating the folders and files

--- Docker_Jenkins/App.java
+++ Docker_Jenkins/App.java
@@ -1,6 +1,6 @@
 // Simple Java Application
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Dockerized Java Application!");
+        System.out.println("Hello, This is Java Application!");
     }
 }
